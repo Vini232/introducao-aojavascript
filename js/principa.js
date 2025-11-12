3b8bfa91b15288fe3be673f58e8661fc5ddd8d66
Vini232 <v.alves21@escola.pr.gov.br>
2025-11-12 10:49:04 -0300

    paciente js

---
+++ js/principa.js
@@ -0,0 +1,10 @@
+
+
+var titulo = document.querySelector(".titulo");
+
+//console.log("titulo");
+//console.log("titulo.textContent");
+
+titulo.textContent = "Aparecida nutricionista"
+
+/* IMC = massa/altura^2  */
