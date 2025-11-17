79a28e347a664003c2617bbc5cf7e8b868b73dd1
Vini232 <v.alves21@escola.pr.gov.br>
2025-11-17 09:40:33 -0300

    validações

--- js/principal.js
+++ js/principal.js
@@ -7,11 +7,34 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista"
 
 /* IMC = massa/altura^2  */
+var tdImc = paciente.querySelector(".info-imc");
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
+
+var pesoEhvalido = true;
+var alturaEhvalida = true;
+// validar o peso
+if (peso <=0 || peso > 1000) {
+    console.log ("Peso inválido")
+    pesoEhvalido = false;
+    tdImc.textContent = "Peso inválido";
+}
+//validar altura
+if (altura <=0 || altura >3) {
+    console.log ("Altura invalida")
+    alturaEhvalida = false;
+    tdImc.textContent = "Altura invalida";
+}
+
+//validar peso e altura
+if (alturaEhvalida && pesoEhvalido) {
+    var imc = peso / (altura*peso);
+    tdImc.textContent = imc;
+}
+
 var imc = peso / (altura*altura);
 
 console.log(paciente); //tr
@@ -20,5 +43,5 @@ console.log(tdAltura); //td que tem altura
 console.log(peso); // valor do peso
 console.log(altura); // valor da altura
 
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+
+
